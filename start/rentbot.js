ca7ddc7dff03297d3d9d87388bd1807e2074574d
the-void-laboratory <thevoidslab16@gmail.com>
2026-05-22 16:36:21 +0100

    hmm

--- start/rentbot.js
+++ start/rentbot.js
@@ -14,7 +14,6 @@ const {
     generateMessageID,
     downloadContentFromMessage,
     makeCacheableSignalKeyStore,
-    // Removed from here to prevent the TypeError
     jidDecode,
     proto,
     Browsers,
